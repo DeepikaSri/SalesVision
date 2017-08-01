1e0ba63c4f6e3e5cc6574553d86109a1868e26c8
Deepika Srimnthula <deepika.srimanthula@wavemaker.com>
2017-08-01 08:58:24 +0000

    Changes Files modified - [MyJavaService6]

--- services/MyJavaService6/src/com/salesvision2_0/myjavaservice6/MyJavaService6.java
+++ services/MyJavaService6/src/com/salesvision2_0/myjavaservice6/MyJavaService6.java
@@ -41,6 +41,7 @@ public class MyJavaService6 {
      *
      * Methods in this class can declare HttpServletRequest, HttpServletResponse as input parameters to access the
      * caller's request/response objects respectively. These parameters will be injected when request is made (during API invocation).
+     * 
      */
     public String sampleJavaOperation(String name, HttpServletRequest request) {
        return "RequestSuccessfull";
